d68e0d5a45a0ef65d4b0b4d37300ed9ee4fedbcb
nikhilwandhare <nikhilwandhare7@gmail.com>
2020-07-13 17:45:20 +0530

    aaa

---
+++ assets/data/treeData.js
@@ -0,0 +1,64 @@
+var tree=[{"module_id": 1,
+			"module_name" : "Administration",
+			"module_logo_class" : null,
+			"menu" : [{"menu_id" : 1,
+						"menu_name" : "Master",
+						"parent_id" : null,
+						"module_id" : 1,
+						"menu_type" : "M",
+						"menu" : [{"menu_id" : 2,
+									"menu_name" : "User Master",
+									"parent_id" : 1,
+									"module_id" : 1,
+									"menu_type" : "F",
+								 },
+								 {
+									"menu_id" : 7,
+									"menu_name" : "Module Master",
+									"parent_id" : 1,
+									"module_id" : 1,
+									"menu_type" : "F"
+								 }]
+					},
+					{"menu_id" : 3,
+					"menu_name" : "Transaction",
+					"parent_id" : null,
+					"module_id" : 1,
+					"menu_type" : "M",
+					"menu" : null
+					}]
+		},
+		{	"module_id" : 2,
+			"module_name" : "Store Management",
+			"module_logo_class" : null,
+			"menu" : [{
+						"menu_id" : 4,
+						"menu_name" : "Master",
+						"parent_id" : null,
+						"module_id" : 2,
+						"menu_type" : "M",
+						"menu" : [{
+									"menu_id" : 5,
+									"menu_name" : "Product Master",
+									"parent_id" : 4,
+									"module_id" : 2,
+									"menu_type" : "F"
+								 }]
+					},
+					{
+						"menu_id" : 5,
+						"menu_name" : "Transaction",
+						"parent_id" : null,
+						"module_id" : 2,
+						"menu_type" : "M",
+						"menu" : null
+					},
+					{
+						"menu_id" : 6,
+						"menu_name" : "Reports",
+						"parent_id" : null,
+						"module_id" : 2,
+						"menu_type" : "M",
+						"menu" : null
+					}]
+		}];
